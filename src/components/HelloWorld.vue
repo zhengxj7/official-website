f133989ff6cc5ca45abd4b12baa17f02039eb0b4
zhengxj <1450734240@qq.com>
2018-10-15 21:51:52 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    pppp
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  props: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+div {
+  color: red;
+}
+</style>
